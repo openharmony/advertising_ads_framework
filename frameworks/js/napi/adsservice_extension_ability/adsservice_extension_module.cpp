a4471f20e0d08be0e571cf4014985b21f0c07511
VioletHan <hanhaiyang1@huawei.com>
2023-10-26 11:07:55 +0800

    Description: add AdsServiceExtensionAbility Feature or BugFix: feature Binary Source: NA
    Signed-off-by: VioletHan <hanhaiyang1@huawei.com>

---
+++ frameworks/js/napi/adsservice_extension_ability/adsservice_extension_module.cpp
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "native_engine/native_engine.h"
+
+extern const char _binary_adsservice_extension_js_start[];
+extern const char _binary_adsservice_extension_js_end[];
+extern const char _binary_adsservice_extension_abc_start[];
+extern const char _binary_adsservice_extension_abc_end[];
+
+static napi_module _module = {
+    .nm_version = 0,
+    .nm_filename = "advertising/libadsserviceextensionability.so/AdsServiceExtensionAbility.js",
+    .nm_modname = "advertising.AdsServiceExtensionAbility",
+};
+extern "C" __attribute__((constructor))
+void NAPI_advertising_AdsServiceExtensionAbility_AutoRegister()
+{
+    napi_module_register(&_module);
+}
+
+extern "C" __attribute__((visibility("default")))
+void NAPI_advertising_AdsServiceExtensionAbility_GetJSCode(const char **buf, int *bufLen)
+{
+    if (buf != nullptr) {
+        *buf = _binary_adsservice_extension_js_start;
+    }
+
+    if (bufLen != nullptr) {
+        *bufLen = _binary_adsservice_extension_js_end - _binary_adsservice_extension_js_start;
+    }
+}
+
+// adsservice extension JS register
+extern "C" __attribute__((visibility("default")))
+void NAPI_advertising_AdsServiceExtensionAbility_GetABCCode(const char **buf, int *buflen)
+{
+    if (buf != nullptr) {
+        *buf = _binary_adsservice_extension_abc_start;
+    }
+    if (buflen != nullptr) {
+        *buflen = _binary_adsservice_extension_abc_end - _binary_adsservice_extension_abc_start;
+    }
+}
