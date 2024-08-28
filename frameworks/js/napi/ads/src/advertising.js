c6694c869d9c92da857470f6e927ec6c1b0328e2
chenshuai <chenshuai3@huawei.com>
2024-08-28 16:58:01 +0800

    解决rpc字符串超长及中继攻击问题
    Signed-off-by: chenshuai <chenshuai3@huawei.com>

--- frameworks/js/napi/ads/src/advertising.js
+++ frameworks/js/napi/ads/src/advertising.js
@@ -150,7 +150,8 @@ class AdsJsClientRpcObj extends rpc.RemoteObject {
       return false;
     }
     try {
-      const readstr = data.readString();
+      const buffer = data.readStringArray();
+      const readstr = buffer.join('');
       this.callback(readstr);
       return true;
     } catch (e) {
@@ -172,34 +173,10 @@ class AdsJsBridge {
       return;
     }
 
-    let options = {
-      onConnect(elementName, remote) {
-        try {
-          // 拼接发送给服务端的数据
-          let data = rpc.MessageSequence.create();
-          data.writeRemoteObject(new AdsJsClientRpcObj('com.ohos.AdsJsClientRpcObj', callback));
-          data.writeString(method);
-          data.writeString(arg);
-          const reply = rpc.MessageSequence.create();
-          const option = new rpc.MessageOption();
-
-          remote.sendMessageRequest(JS_BRIDGE_RPC_CODE, data, reply, option)
-            .catch((e) => {
-              hilog.error(HILOG_DOMAIN_CODE, 'advertising', `sendMessageRequest error, code:${e.code}, message:${e.message}`);
-            })
-            .finally(() => {
-              data.reclaim();
-              reply.reclaim();
-            });
-        } catch (e) {
-          hilog.error(HILOG_DOMAIN_CODE, 'advertising', `onConnect error, code:${e.code}, message:${e.message}`);
-        }
-      },
-      onDisconnect() {
-      },
-      onFailed() {
-      }
-    };
+    if (arg.length > PARSE_RESP_LENGTH_LIMIT) {
+      hilog.error(HILOG_DOMAIN_CODE, 'advertising', `invokeAsync parameter arg is too long`);
+      return;
+    }
 
     try {
       const map = getConfigJsonData();
@@ -210,16 +187,54 @@ class AdsJsBridge {
         callback(null);
         return;
       }
+
+      const options = createJsbOptions(method, arg, callback);
       const want = {
         bundleName: bundleName,
         abilityName: abilityName
       };
-
       this.context.connectServiceExtensionAbility(want, options);
     } catch (e) {
       callback(null);
       hilog.error(HILOG_DOMAIN_CODE, 'advertising', `invokeAsync error, code:${e.code}, message:${e.message}`);
     }
+  }
+}
+
+function createJsbOptions(method, arg, callback) {
+  return {
+    onConnect(elementName, remote) {
+      sendJsbRpcMessage(method, arg, callback, remote);
+    },
+    onDisconnect() {
+    },
+    onFailed() {
+    }
+  };
+}
+
+function sendJsbRpcMessage(method, arg, callback, remote) {
+  try {
+    // 拼接发送给服务端的数据
+    let data = rpc.MessageSequence.create();
+    hilog.info(HILOG_DOMAIN_CODE, 'advertising', `remote descriptor:${remote.getDescriptor()}`);
+    data.writeInterfaceToken(remote.getDescriptor());
+    data.writeRemoteObject(new AdsJsClientRpcObj('com.ohos.AdsJsClientRpcObj', callback));
+    data.writeString(method);
+    data.writeStringArray(splitString((arg), RPC_STRING_LENGTH));
+    const reply = rpc.MessageSequence.create();
+    const option = new rpc.MessageOption();
+
+    remote.sendMessageRequest(JS_BRIDGE_RPC_CODE, data, reply, option)
+      .catch((e) => {
+        hilog.error(HILOG_DOMAIN_CODE, 'advertising', `sendMessageRequest error, code:${e.code}, message:${e.message}`);
+      })
+      .finally(() => {
+        data.reclaim();
+        reply.reclaim();
+      });
+  } catch (e) {
+    hilog.error(HILOG_DOMAIN_CODE, 'advertising', `sendJsbRpcMessage error, code:${e.code}, message:${e.message}`);
   }
 }
 
@@ -332,6 +347,8 @@ function createConnectionOptions(listener, adResponse) {
 function sendRPCMessage(listener, adResponse, remote) {
   try {
     let data = rpc.MessageSequence.create();
+    hilog.info(HILOG_DOMAIN_CODE, 'advertising', `remote descriptor:${remote.getDescriptor()}`);
+    data.writeInterfaceToken(remote.getDescriptor());
     data.writeRemoteObject(new ParseAdResponseRpcObj('com.ohos.ParseAdResponseRpcObj', listener));
     data.writeStringArray(splitString(adResponse, RPC_STRING_LENGTH));
 
@@ -374,7 +391,7 @@ function getConfigData() {
 }
 
 function splitString(str, length) {
-  let result = [''];
+  let result = [];
   for (let i = 0; i < str.length; i += length) {
     result.push(str.slice(i, i + length));
   }
