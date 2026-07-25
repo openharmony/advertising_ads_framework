c958520c0ad55edb7163a27811e11541b963d017
qq_35015309 <1257369706@qq.com>
2026-07-25 08:57:38 +0800

    update: 更新文件 advertising.h
    Signed-off-by: qq_35015309 <1257369706@qq.com>

--- frameworks/js/napi/ads/include/advertising.h
+++ frameworks/js/napi/ads/include/advertising.h
@@ -112,9 +112,9 @@ private:
 
     static napi_value LoadAdWithMultiSlots(napi_env env, napi_callback_info info);
 
-    bool ParseMultiSlotsRequest(napi_env env, napi_value argv[], MultiSlotsRequestContext *context);
+    static bool ParseMultiSlotsRequest(napi_env env, napi_value argv[], MultiSlotsRequestContext *context);
 
-    bool ParseMultiSlotsOption(napi_env env, napi_value argv[], MultiSlotsRequestContext *context);
+    static bool ParseMultiSlotsOption(napi_env env, napi_value argv[], MultiSlotsRequestContext *context);
      
     static napi_value ParseContextForMultiSlots(napi_env env, napi_callback_info info,
         MultiSlotsRequestContext *context);
